1b59e072a7e9b264acd1dcd00867d22e829c832f
GyubinHan <ghan3@e8ight.co.kr>
2022-02-16 15:18:19 +0900

    Fibonacci test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+var FibonacciNumbers = new List<int> {1,1};
+
+
+
+while(FibonacciNumbers.Count < 21)
+{   
+    var prev1 = FibonacciNumbers[FibonacciNumbers.Count -1];
+    var prev2 = FibonacciNumbers[FibonacciNumbers.Count -2];
+    FibonacciNumbers.Add(prev1 + prev2);
+
+}
+
+
+foreach (var n in FibonacciNumbers)
+{
+    Console.WriteLine(n);
+}
